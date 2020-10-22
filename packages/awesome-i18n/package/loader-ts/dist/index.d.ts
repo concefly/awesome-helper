89dda1ce4a8ff9582aee705830f88987e16805be
concefly <concefly@foxmail.com>
2020-10-22 11:12:10 +0800

    feat: 迁移 awesome-i18n

--- packages/awesome-i18n/package/loader-ts/dist/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { LoadResult, BaseLoader } from 'ai18n-type';
-export default class Loader extends BaseLoader {
-    /** 解析函数表达式 */
-    private parseCallExpression;
-    parse(code: string, filePath: string): LoadResult;
-}
--- packages/awesome-i18n/package/loader-ts/dist/walk.d.ts
+++
@@ -1,2 +0,0 @@
-import * as ts from 'typescript';
-export declare function walk(node: ts.Node, callback: (n: ts.Node) => void): void;
