9098334c3e3a3434743be7bab9412b343292a66a
concefly <concefly@foxmail.com>
2020-12-07 13:00:47 +0800

    fix: fix koa plugin type

--- packages/ah-server/src/index.ts
+++ packages/ah-server/src/index.ts
@@ -5,7 +5,17 @@ export interface IApplication {}
 export interface IService {}
 
 export interface IContext extends ParameterizedContext<DefaultState, DefaultContext> {}
-export interface IConfig {}
+export interface IConfig {
+  LOCAL_PORT: number;
+}
+
+// fix koa plugin type
+declare module 'koa' {
+  interface Request {
+    // koaBody type
+    body?: any;
+  }
+}
 
 export * from './App';
 export * from './Config';
